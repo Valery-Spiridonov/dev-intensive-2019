6ab314f819c0f6e3c744b31bab8d021cd47e2068
valery_spiridonov_rdlab <spiridonov@rdlab.info>
2019-07-11 16:39:16 +0300

    [Hometask_2] corrected error: java.lang.AssertionError: expected: null<null> but was: java.lang.String<null>

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        val parts: List<String>? = fullName?.split(" ")
+        val parts: List<String>? = if(fullName.isNullOrBlank()) null else fullName?.split(" ")
         val firstName = parts?.getOrNull(0)
         val lastName= parts?.getOrNull(1)
 
@@ -17,7 +17,7 @@ object Utils {
         val firstInitial = "${if(firstName.isNullOrBlank()) "" else firstName?.capitalize().first()}"
         val lastInitial = "${if(lastName.isNullOrBlank()) "" else lastName?.capitalize().first()}"
 
-        return "${if("$firstInitial$lastInitial".isNullOrBlank()) null else "$firstInitial$lastInitial"}"
+        return if("$firstInitial$lastInitial".isNullOrBlank()) null else "$firstInitial$lastInitial"
     }
 
 
